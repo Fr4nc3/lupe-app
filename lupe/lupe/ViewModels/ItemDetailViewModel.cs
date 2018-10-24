6284eacfcf7fccdfc3a832dd5617c296c6e5c0ec
Fr4nc3 <ff2214@columbia.edu>
2018-10-24 12:47:58 -0400

    Lupe with xamarin

---
+++ lupe/lupe/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using lupe.Models;
+
+namespace lupe.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
